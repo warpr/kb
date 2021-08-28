bb1fc9e83514b9f91f08dacbd8ffbd8ab75aa00b
Kuno Woudt <kuno@frob.nl>
2021-08-28 18:10:30 -0500

    App shortcuts

--- index.php
+++ index.php
@@ -1,101 +1,122 @@
 <?php
 
-function str_starts_with ( $haystack, $needle ) {
-    return strpos( $haystack , $needle ) === 0;
+function str_starts_with($haystack, $needle)
+{
+    return strpos($haystack, $needle) === 0;
 }
 
-$thumb = [ null, null, null, '⌥', '⌘', "⎵\t\tLower", null, "⏎\t\tRaise", "⌘", '⌥', null, null, null ];
+$thumb = [null, null, null, "\t\t⌥", "\t\t⌘", "⎵\t\tLower", null, "⏎\t\tRaise", "\t\t⌘", "\t\t⌥", null, null, null];
 
 $dvorak = [
-    [ 'TAB', "'", ",", ".", "p", "y", null, "f", "g", "c", "r", "l", "ESC" ],
-    [ "\t\tCTRL", 'a', 'o', 'e', 'u', 'i', null, 'd', 'h', 't', 'n', 's', "-\t_" ],
-    [ "&nbsp;(\t\t⇧", ";", 'q', 'j', 'k', 'x', null, 'b', 'm', 'w', 'v', 'z', "&nbsp;)\t\t⇧" ],
+    ['TAB', "'\t\"", ",\t<", ".\t>", 'P', 'Y', null, 'F', 'G', 'C', 'R', 'L', 'ESC'],
+    ["\t\tCTRL", 'A', 'O', 'E', 'U', 'I', null, 'D', 'H', 'T', 'N', 'S', "-\t_"],
+    ["&nbsp;(\t\t⇧", ";\t:", 'Q', 'J', 'K', 'X', null, 'B', 'M', 'W', 'V', 'Z', "&nbsp;)\t\t⇧"],
     $thumb,
 ];
 
 $lower = [
-    [ "<", "1", "2", "3", "4", "5", null, "6", "7", "8", "9", "0", '>' ],
-    [ '{', '!', '@', '#', '$', '%', null, '?', '/', '=', '\\', '|', '}' ],
-    [ '[', '', ':', '`', '~', '"',  null, '^', '&', '*', '+', '', ']', ],
+    ['`', '1', '2', '3', '4', '5', null, '6', '7', '8', '9', '0', '⌫'],
+    ['{', '!', '@', '#', '$', '%', null, '?', '/', '=', '\\', '|', '}'],
+    ['[', '', '', '', '~', '', null, '^', '&', '*', '+', '', ']'],
     $thumb,
 ];
 
 $raise = [
-    [ '', '', '', '', '', '', null, 'EMACS. C-x [', 'EMACS. M-b', '↑', 'EMACS. M-f', 'Page Up', 'Home' ],
-    [ '', '', '', '', '', '', null, 'EMACS. C-x ]', '←', '↓', '→', 'Page Down', 'End' ],
-    [ '', '', '', '', '', '', null, '', 'MACRO. Moom', 'DEL', '⌫', '', '' ],
+    ['', '', '', 'MACRO. ⌘⇧3', 'MACRO. ⌘⇧4', 'MACRO. ⌘⇧5', null, 'Page Up', 'EMACS. M-b', '↑', 'EMACS. M-f', 'Home', '⌫'],
+    ['', '', '', '', '', '', null, 'Page Down', '←', '↓', '→', 'End', ''],
+    ['', '', '', '', '', '', null, 'MOOM. ⎈⌘M', 'EMACS. C-x [', 'DEL', 'EMACS. C-x ]', '', ''],
     $thumb,
 ];
 
 $accents = [
-    [ 'ò', 'æ', '≤', '≥', 'π', '\\', null, 'ƒ', '©', 'ç', '®', '¬', ' ' ],
-    [ ' ', 'å', 'ø', 'ó', 'ö', 'ô', null,  '∂', '˙', '†', 'ñ', 'ß', '–' ],
-    [ ' ', '…', 'œ', '∆', '˚', '≈', null,  '∫', 'µ', '∑', '√', 'Ω', ' ' ],
+    ['ù', 'DIM. æ', 'DIM. ≤', 'DIM. ≥', 'DIM. π', 'DIM. \\', null, 'DIM. ƒ', 'DIM. ©', 'DIM. ç', 'DIM. ®', 'DIM. ¬', ''],
+    ['DIM.  ', 'DIM. å', 'DIM. ø', 'é', 'ü', 'î', null, 'DIM. ∂', 'DIM. ˙', 'DIM. †', 'ñ', 'DIM. ß', ''],
+    ['DIM.  ', 'DIM. …', 'DIM. œ', 'DIM. ∆', 'DIM. ˚', 'DIM. ≈', null, 'DIM. ∫', 'DIM. µ', 'DIM. ∑', 'DIM. √', 'DIM. Ω', 'DIM.  '],
+    $thumb,
+];
+
+$shortcuts = [
+    ['', '', '', '', '', '', null, '', '', 'fab-chrome', '1pass', '', ''],
+    ['', '', '', '', '', '', null, 'fab-discord', 'emacs', 'fas-terminal', 'fab-firefox-browser', 'fab-safari', ''],
+    ['', '', '', '', '', '', null, 'picard', '', '', "virtual\nbox", 'zoom', ''],
     $thumb,
 ];
 
 $layers = [
-    "dovark" => $dvorak,
-    "lower" => $lower,
-    "raise" => $raise,
-    "accents (macos)" => $accents,
+    'Dvorak' => $dvorak,
+    'Lower' => $lower,
+    'Raise' => $raise,
+    'MacOS ⌥' => $accents,
+    '⎈+⌘' => $shortcuts,
 ];
 
-function render_key($key) {
+function render_key($key)
+{
     if ($key === null) {
-        echo "<key-placeholder> </key-placeholder>";
+        echo '<key-placeholder> </key-placeholder>';
         return;
     }
 
-    $parts = explode("\t",$key);
+    $parts = explode("\t", $key);
 
-    echo "<key-cap>";
+    echo '<key-cap>';
 
-    if (preg_match("/^EMACS\.(.*)$/", $parts[0], $matches)) {
-        echo "<key-macro>" . trim($matches[1]) . "</key-macro>";
-        echo "<key-emacs>emacs</key-emacs>";
-    } else if (preg_match("/^MACRO\.(.*)$/", $parts[0], $matches)) {
-        echo "<key-macro>" . trim($matches[1]) . "</key-macro>";
-        echo "<key-emacs>macro</key-emacs>";
+    if (preg_match("/^fab-(.*)$/", $parts[0], $matches)) {
+        echo '<key-tap class="big"><i class="fab fa-'. $matches[1] . '"></i></key-macro>';
+    } else if (preg_match("/^fas-(.*)$/", $parts[0], $matches)) {
+        echo '<key-tap class="big"><i class="fas fa-'. $matches[1] . '"></i></key-macro>';
+    } else if (preg_match("/^DIM\.(.*)$/", $parts[0], $matches)) {
+        echo '<key-tap class="big dim">' . trim($matches[1]) . '</key-tap>';
+    } elseif (preg_match("/^(([A-Z]+)\. )(.*)$/", $parts[0], $matches)) {
+        $grp = strtolower(trim($matches[2]));
+        echo "<key-macro class='" . $grp . "'>" . trim($matches[3]) . '</key-macro>';
+        echo "<key-macro-desc class='" . $grp . "'>" . $grp . '</key-macro-desc>';
+    } elseif (
+        count($parts) == 1 &&
+        (mb_strlen(trim($parts[0])) == 1 || $parts[0] == 'é' || $parts[0] == 'ü' || $parts[0] == 'î' || $parts[0] == 'ù')
+    ) {
+        echo '<key-tap class="big">' . trim($parts[0]) . '</key-tap>';
+    } elseif (count($parts) == 2 && mb_strlen(trim($parts[0])) == 1 && mb_strlen(trim($parts[1])) == 1) {
+        echo '<key-tap class="big-unshifted">' . trim($parts[0]) . '</key-tap>';
     } else {
-        echo "<key-tap>" . $parts[0] . "</key-tap>";
+        echo '<key-tap>' . $parts[0] . '</key-tap>';
     }
 
     if (!empty($parts[1])) {
-        echo "<key-shifted>" . $parts[1] . "</key-shifted>";
+        echo '<key-shifted>' . $parts[1] . '</key-shifted>';
     }
     if (!empty($parts[2])) {
-        echo "<key-hold>" . $parts[2] . "</key-hold>";
+        echo '<key-hold>' . $parts[2] . '</key-hold>';
     }
     echo "</key-cap>\n";
 }
 
-function render_layer($name, $data) {
-    echo "<layer-label><h1>$name</h1></layer-label>";
+function render_layer($name, $data)
+{
+    echo '<layer-box>';
     foreach ($data as $row) {
-        echo "<key-row>";
+        echo '<key-row>';
         foreach ($row as $key) {
             render_key($key);
         }
-        echo "</key-row>";
+        echo '</key-row>';
     }
+    echo "<layer-label><h1>$name</h1></layer-label>";
+    echo '</layer-box>';
 }
-
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Keyboard Layout</title>
         <style>
-            <?php require_once('style.css'); ?>
+            <?php require_once 'style.css'; ?>
         </style>
+        <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.css" />
     </head>
     <body>
-   <?php
-        foreach ($layers as $name => $data) {
-            render_layer($name, $data);
-            echo "<br/><br/>";
-        }
-   ?>
+   <?php foreach ($layers as $name => $data) {
+       render_layer($name, $data);
+   } ?>
     </body>
 </html>
